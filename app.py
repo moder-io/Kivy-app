3a8d926f359044d5f872f32ce717951666fab638
moder-io <rfrancisco100pro@gmail.com>
2024-10-12 18:39:54 +0200

    app.py

--- app.py
+++ app.py
@@ -1,77 +1,57 @@
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, RiseInTransition
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
-import webbrowser, subprocess, logging, os
+import webbrowser
+import subprocess
+import logging
+import os
 
+logging.basicConfig(filename='logs.log', level=logging.INFO, 
+                    format='%(asctime)s - %(message)s')
 
-logger = logging.getLogger() 
-logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs.log')
-formatter = logging.Formatter('%(asctime)s - %(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-
-class Ui(ScreenManager): 
+class Ui(ScreenManager):
     pass
 
-
-class MiPopup(Popup):
+class WelcomePopup(Popup):
     pass
-
 
 class MainApp(MDApp):
-
     icon = "images/icon.png"
-    title = "App"
-
-    def encript(self):
-        ruta_script = "encriptador/encriptador.py"
-        subprocess.Popen(["python", ruta_script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        logger.info('Usuario encripto')
-
-    def desencript(self):
-        ruta_script = "encriptador/desencriptar.py"
-        subprocess.Popen(["python", ruta_script], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        logger.info('Usuario desencripto')
-
-    def terminal(self):
-       os.system("start cmd.exe")
-       logger.info("Terminal abierto")
-
-    def powerShell(self):
-       os.system("start powershell.exe")
-       logger.info("Power Shell abierto")
-
-    def webOpen(self):
-        url = "https://github.com"
-        webbrowser.open(url)
-        logger.info("github abierto")
-
-    def visual(self):
-        code_path = "C:/Users/rfran/AppData/Local/Programs/Microsoft VS Code/Code.exe"
-        os.system(f'start "" "{code_path}"')
-        logger.info("Visual Studio Code abierto")
+    title = "Enhanced App"
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Teal"
-        Builder.load_file("desing.kv")
-        return Ui() 
+        return Builder.load_file("design.kv")
 
-    def popup(self):
-        popup = MiPopup()
+    def on_start(self):
+        self.show_welcome_popup()
+
+    def show_welcome_popup(self):
+        popup = WelcomePopup()
         popup.open()
-        logger.info("popup abierto")
+        logging.info("Welcome popup opened")
 
-    def exit(self):
+    def run_script(self, script_name):
+        script_path = os.path.join("scripts", f"{script_name}.py")
+        subprocess.Popen(["python", script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        logging.info(f'User ran {script_name} script')
+
+    def open_terminal(self, terminal_type):
+        os.system(f"start {terminal_type}")
+        logging.info(f"{terminal_type} opened")
+
+    def open_website(self, url):
+        webbrowser.open(url)
+        logging.info(f"Opened website: {url}")
+
+    def open_application(self, app_path):
+        os.system(f'start "" "{app_path}"')
+        logging.info(f"Opened application: {app_path}")
+
+    def exit_app(self):
         self.stop()
 
-
 if __name__ == "__main__":
-    app = MainApp()
-    app.run()
-
-
-logging.shutdown() 
+    MainApp().run()
