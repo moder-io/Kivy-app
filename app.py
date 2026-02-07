b6eef03ea9796504b31cd914a331869da9826d57
moder-io <rfrancisco100pro@gmail.com>
2026-02-07 10:46:13 +0100

    1.1 -> Modified .kivy

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+import os
+import sys
+from pathlib import Path
+
+
+if sys.platform.startswith("win"):
+    base = Path(os.getenv("LOCALAPPDATA", Path.home()))
+    os.environ["KIVY_HOME"] = str(base / "App")
+else:
+    os.environ["KIVY_HOME"] = str(Path.home() / ".App")
+
+
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
-import webbrowser, logging, os, sys
+import webbrowser, logging
 
 
-logger = logging.getLogger() 
+logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler('logs.log')
 formatter = logging.Formatter('%(asctime)s - %(message)s')
@@ -13,8 +25,7 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 
-
-class Ui(ScreenManager): 
+class Ui(ScreenManager):
     pass
 
 
@@ -26,49 +37,39 @@ class MainApp(MDApp):
     title = "App"
 
     def resource_path(self, relative_path):
-        try:
-            base_path = sys._MEIPASS  # PyInstaller
-        except Exception:
-            base_path = os.path.abspath(".")
-        return os.path.join(base_path, relative_path)
-
+        base_path = Path(getattr(sys, "_MEIPASS", Path(__file__).parent))
+        return str(base_path / relative_path)
 
     def terminal(self):
-       os.system("start cmd.exe")
-       logger.info("Terminal abierto")
-
+        os.system("start cmd.exe")
+        logger.info("Terminal abierto")
 
     def powerShell(self):
-       os.system("start powershell.exe")
-       logger.info("Power Shell abierto")
-
+        os.system("start powershell.exe")
+        logger.info("Power Shell abierto")
 
     def webOpen(self):
         url = "https://github.com"
         webbrowser.open(url)
         logger.info("github abierto")
 
-
     def visual(self):
         os.system("code .")
         logger.info("Visual Studio Code abierto")
-
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Teal"
         self.icon = self.resource_path("images/icon.png")
         Builder.load_file(self.resource_path("design.kv"))
-        return Ui() 
-
+        return Ui()
 
     def popup(self):
         popup = MiPopup()
         popup.open()
         logger.info("popup abierto")
 
-
-    def exit(self): 
+    def exit(self):
         self.stop()
 
 
@@ -76,5 +77,4 @@ if __name__ == "__main__":
     app = MainApp()
     app.run()
 
-
-logging.shutdown() 
+logging.shutdown()
